08fc8f4f83bd969ae220fcdf4547e09c2af50c9e
heloisethero <thero.heloise@gmail.com>
2020-05-03 16:50:47 +0200

    display the good dice on each casino

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,65 @@
+function numberToString(number) {
+  switch(number) {
+    case "1":
+      return "one";
+    case "2":
+      return "two";
+    case "3":
+      return "three";
+    case "4":
+      return "four";
+    case "5":
+      return "five";
+    case "6":
+      return "six";
+    default:
+      return "one";
+  }
+}
+
 $(function () {
   var socket = io();
+
+  updateCasinos = function(casinos){
+
+    $("#casinosRow").empty();
+
+    for (var casino in casinos){
+      var strv = `  <div class="card" style="width: 25rem;">
+        <div class="card-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-4">
+                <h5 class="card-title"><i style="font-size: 4em; color: grey;" class="fas fa-dice-`+ numberToString(casino) +`"></i></h5>
+                <h6 class="card-subtitle mb-2 text-muted">90 $ <br> 20 $ <br> 0 $</h6>
+              </div>
+              <div class="col-sm-8">
+                <p class="card-text">
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one black"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one black"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one black"></i>
+                  <br>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one red"></i>                
+                  <br>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                  <i style="font-size: 1.5em;" class="fas fa-dice-one green"></i>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+      $("#casinosRow").append(strv);
+    }
+  };
+
   socket.on('playersUpdate', function(players){
     $("#playersList").empty();
     for (var p in players) {
@@ -31,6 +91,7 @@ $(function () {
   });
 
   socket.on('gameStarted', function(casinos){
+    updateCasinos(casinos);
     $('#startRow').hide();
     $('#casinosRow').show();
     $('#diceRow').show();
