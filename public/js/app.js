7a5a1cc26e67e6bf25b9c522bd5768aece683315
heloisethero <thero.heloise@gmail.com>
2020-05-03 18:42:49 +0200

    fix Antoine's bug

--- public/js/app.js
+++ public/js/app.js
@@ -152,8 +152,9 @@ $(function () {
 
     // il faut faire socket.emit('rollDices');
   });
+  
   socket.on('dicesRolled', function(rolledDices, currentPlayerId){
 
     // il faut faire socket.emit('placeDices', nbr);
-  };
+  });
 });
